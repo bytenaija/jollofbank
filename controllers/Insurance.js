7ed899ebaf4dd9d5ab384ebddfe8c9d7d4c12ee4
Everistus Olumese <everistusolumese@gmail.com>
2018-08-25 06:54:27 +0100

    Setting up 2

--- controllers/Insurance.js
+++ controllers/Insurance.js
@@ -86,7 +86,7 @@ module.exports = {
     },
 
     travel: (req, res) => {
-        console.dir(req.body.queryResult.intent)
+        console.dir(req.body.queryResult.intent.displayName)
             /* switch (req.body.result.displayName) {
                 case 'Life Insurance':
                     console.log("Life Insurance")
