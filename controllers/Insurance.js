ea1a78f039f626ca5a882aa183dbb1b1208961f1
Everistus Olumese <everistusolumese@gmail.com>
2018-08-25 05:06:47 +0100

    Setting up 2

--- controllers/Insurance.js
+++ controllers/Insurance.js
@@ -1,17 +1,33 @@
 module.exports = {
     car: (req, res) => {
-        console.dir(req)
+        console.dir(req.body)
+        res.json({
+            "speech": "Your car insurance needs have been served",
+            "displayText": "Your car insurance needs have been served"
+        })
     },
 
     travel: (req, res) => {
-        console.dir(req)
+        console.dir(req.body)
+        res.json({
+            "speech": "Your travel insurance needs have been served",
+            "displayText": "Your travel insurance needs have been served"
+        })
     },
 
     home: (req, res) => {
-        console.dir(req)
+        console.dir(req.body)
+        res.json({
+            "speech": "Your home insurance needs have been served",
+            "displayText": "Your home insurance needs have been served"
+        })
     },
 
     life: (req, res) => {
-        console.dir(req)
+        console.dir(req.body)
+        res.json({
+            "speech": "Your life insurance needs have been served",
+            "displayText": "life travel insurance needs have been served"
+        })
     },
 }
