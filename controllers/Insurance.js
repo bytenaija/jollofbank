2f0ac08a4b6f5bd7a3b8f5a9b3ab31fe2adab19b
Everistus Olumese <everistusolumese@gmail.com>
2018-08-27 15:57:30 +0100

    Actions

--- controllers/Insurance.js
+++ controllers/Insurance.js
@@ -398,7 +398,7 @@ function travelInsurance(req, res) {
 module.exports = {
 
     insurance: (req, res) => {
-        const agent = new WebhookClient({ req, res });
+        const agent = new WebhookClient({ request: req, respons: res });
         console.dir(agent);
         let outputContexts = req.body.queryResult.outputContexts;
         //console.dir(outputContexts[outputContexts.length - 1].parameters)
