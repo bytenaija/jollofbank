0c93df654db87608825655755f8a7edee095697d
Everistus Olumese <everistusolumese@gmail.com>
2018-11-02 14:56:21 +0100

    Added Jollof Bank

--- controllers/jollofbank.js
+++ controllers/jollofbank.js
@@ -52,13 +52,13 @@ function rechargePhone(agent) {
                 if (account) {
                     console.log('Account balance', account)
                     if (account.balance >= agent.parameters.amount) {
-                        Transaction.create({ accountId: account.accountId, description: `Purchase of NGN ${agent.parameters.amount} for ${agent.parameters.phoneNo}`, amount: agent.parameters.amount }).then(transaction => {
+                      return Transaction.create({ accountId: account.accountId, description: `Purchase of NGN ${agent.parameters.amount} for ${agent.parameters.phoneNo}`, amount: agent.parameters.amount }).then(transaction => {
                             agent.add(`Airtime of ${agent.parameters.amount} has been successfully purchased for ${agent.parameters.phoneNo}`);
                             resolve(agent)
                         });
 
-                        agent.add(`Airtime of ${agent.parameters.amount} has been successfully purchased for ${agent.parameters.phoneNo}`);
-                        resolve(agent)
+                        // agent.add(`Airtime of ${agent.parameters.amount} has been successfully purchased for ${agent.parameters.phoneNo}`);
+                        // resolve(agent)
                     } else {
                         agent.add(`You do not have sufficient amount in your account to complete this transaction`);
                         resolve(agent)
